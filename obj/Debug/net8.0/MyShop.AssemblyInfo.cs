ee5054c2e1cfdede04f3457cb373516d629026f8
Karianne <113008581+kariannegle@users.noreply.github.com>
2024-10-22 16:42:32 +0200

    Partial view

--- obj/Debug/net8.0/MyShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e56665e78585e609ad3edefc3f715b1c08b313af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffad44a9e76a5e2d1327edc410721c85ff5374b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
