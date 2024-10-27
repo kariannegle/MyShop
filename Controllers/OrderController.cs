3979e7126b40529ccc3c9ea43f1e7c88b0256c8a
Karianne <113008581+kariannegle@users.noreply.github.com>
2024-10-27 21:39:43 +0100

    Expanded CRUD

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,28 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
 using MyShop.Models;
 using Microsoft.EntityFrameworkCore;
+using MyShop.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MyShop.Controllers;
 
 public class OrderController : Controller
 {
-    private readonly ItemDbContext _itemDbContext;
+    private readonly ItemDbContext _itemDbContext; // Private readonly field to hold the ItemDbContext instance
 
-    public OrderController(ItemDbContext itemDbContext)
+    public OrderController(ItemDbContext itemDbContext) // Constructor to inject the ItemDbContext instance
     {
         _itemDbContext = itemDbContext;
     }
 
-    public async Task<IActionResult> Table()
+    public async Task<IActionResult> Table() // Action method to display the orders in a table
     {
         List<Order> orders = await _itemDbContext.Orders.ToListAsync();
         return View(orders);
     }
 
     [HttpGet]
-    public IActionResult CreateOrderItem()
+    public async Task<IActionResult> CreateOrderItem() // Action method to display the form to create an OrderItem
     {
-        return View();
+        var items = await _itemDbContext.Items.ToListAsync(); // Retrieve all items from the database and convert them to a list
+        var orders = await _itemDbContext.Orders.ToListAsync();
+        var createOrderItemViewModel = new CreateOrderItemViewModel
+        {
+            OrderItem = new OrderItem(),
+
+            ItemSelectList = items.Select(item => new SelectListItem // Wraps the info of ItemIds and Item Names into ItemSelectList
+            {
+                Value = item.ItemId.ToString(),
+                Text = item.ItemId.ToString() + ": " + item.Name
+            }).ToList(),
+
+            OrderSelectList = orders.Select(order => new SelectListItem // wraps the information of OrderIds, OrderDates, and Customer Names into the OrderSelectList.
+            {
+                Value = order.OrderId.ToString(),
+                Text = "Order" + order.OrderId.ToString() + ", Date: " + order.OrderDate + ", Customer: " + order.Customer.Name
+            }).ToList(),
+        };
+        return View(createOrderItemViewModel);
     }
 
     [HttpPost]
