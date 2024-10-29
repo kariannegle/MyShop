6e44a94ad92de3047afbb9bc33b9da9f15b0844d
Karianne <113008581+kariannegle@users.noreply.github.com>
2024-10-30 00:41:59 +0100

    Logging and error handling

--- Program.cs
+++ Program.cs
@@ -1,16 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using MyShop.DAL;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews(); // Add MVC services (components registered with ASP.NET) to the container
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ItemDbContext>(options => {
     options.UseSqlite(
         builder.Configuration["ConnectionStrings:ItemDbContextConnection"]);
 });
 
-builder.Services.AddScoped<IItemRepository, ItemRepository>(); // Register the ItemRepository class with the container
+builder.Services.AddScoped<IItemRepository, ItemRepository>();
+
+var loggerConfiguration = new LoggerConfiguration()
+    .MinimumLevel.Information() // levels: Trace< Information < Warning < Erorr < Fatal
+    .WriteTo.File($"Logs/app_{DateTime.Now:yyyyMMdd_HHmmss}.log");
+
+loggerConfiguration.Filter.ByExcluding(e => e.Properties.TryGetValue("SourceContext", out var value) &&
+                            e.Level == LogEventLevel.Information &&
+                            e.MessageTemplate.Text.Contains("Executed DbCommand"));
+
+var logger = loggerConfiguration.CreateLogger();
+builder.Logging.AddSerilog(logger);
 
 var app = builder.Build();
 
